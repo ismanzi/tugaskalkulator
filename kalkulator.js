febd8fd397b363e223419b20297b511db2b08e36
ISMAN FAUZI <118708678+ismanzi@users.noreply.github.com>
2022-11-29 15:33:00 +0700

    Add files via upload

---
+++ kalkulator.js
@@ -0,0 +1,12 @@
+var input=require('readline-sync');
+console.log (" Enter " + " + " + " - " + " * " + " or " + " / ")
+const input2 = input.question("pilihlah salah satu =")
+console.log("..........................");
+
+if (input2=="/");
+var bil1= input.question("masukkan angka =");
+var bil2= input.question("masukkan angka =");
+var intbil1 = bil1
+var intbil2 = bil2
+var hasil = (Number(bil1)/(Number(bil2)))
+console.log("Hasilnya adalah " + hasil);
