c87f233bab9f0b795f573a1d3af7ff24ab261a14
dwinurfatimah20 <118708533+dwinurfatimah20@users.noreply.github.com>
2022-12-01 11:42:48 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -10,3 +10,17 @@ var intbil1 = bil1
 var intbil2 = bil2
 var hasil = (Number(bil1)/(Number(bil2)))
 console.log("Hasilnya adalah " + hasil);
+
+var input=require('readline-sync');
+console.log ("..........................");
+console.log (" Enter " + " + " + " - " + " * " + " or " + " / ")
+const input2 = input.question("pililah salah satu =")
+console.log("...........................");
+
+if (input2=="*");
+const bil1= input.question("masukkan angka =");
+const bil2= input.question("masukkan angka =");
+const intbil1 = bil1
+const intbil2 = bil2
+const hasil = (Number(bil1)*(Number(bil2)))
+console.log("Hasilnya adalah " + hasil);
