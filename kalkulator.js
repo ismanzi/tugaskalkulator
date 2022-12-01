ee0c9fd1ff36452eee5a9cc0a442bb24f0e3b4fc
ISMAN FAUZI <118708678+ismanzi@users.noreply.github.com>
2022-12-01 12:54:30 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -31,10 +31,20 @@ if (input2=="/"){
 }
 
 if (input2=="*"){
-const bil1= input.question("masukkan angka =");
-const bil2= input.question("masukkan angka =");
-const intbil1 = bil1
-const intbil2 = bil2
-const hasil = (Number(bil1)*(Number(bil2)))
-console.log("Hasilnya adalah " + hasil)
+    var bil1= input.question("masukkan angka =");
+    var bil2= input.question("masukkan angka =");
+    var intbil1 = bil1
+    var intbil2 = bil2
+    var hasil = (Number(bil1)*(Number(bil2)))
+  console.log("Hasilnya adalah " + hasil)
 }
+
+if (input2=="%"){
+    var bil1= input.question("masukkan angka = ");
+    var bil2= input.question("masukkan angka = ");
+    var intbil1 = bil1
+    var intbil2 = bil2
+    var hasil = Number(intbil1)%Number(bil2)
+   console.log("Hasilnya adalah " + hasil)
+}
+    
